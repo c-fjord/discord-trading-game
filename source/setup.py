4ef99d299e5e64e7d8575a4d42ac15422b49e33a
c-fjord <73600781+c-fjord@users.noreply.github.com>
2020-11-12 20:23:14 +0100

    Removed Token

--- source/setup.py
+++ source/setup.py
@@ -2,9 +2,11 @@ from discord.ext import commands
 from source.user_interface import User_interface
 from source.error import ErrorHandler
 
-TOKEN = "NzYzNDU0MDYyMTM2OTE4MDM3.X338AA.6cUqicxu72sbWw0xrIYMtM1ajyI"
-
 if __name__ == '__main__':
+
+    with open(r'..\source\token.txt') as f:
+        TOKEN = f.read()
+
     bot = commands.Bot('?')
     bot.add_cog(User_interface(bot))
     bot.add_cog(ErrorHandler(bot))
